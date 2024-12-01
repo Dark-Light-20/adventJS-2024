c9c26431539bc9d1db2d479fdf05895d222e9c4b
Dark-Light-20 <pazsebas@hotmail.com>
2024-12-01 11:19:05 -0500

    refactor: :zap: Improved code for exercise 01

--- solutions/01.js
+++ solutions/01.js
@@ -1,5 +1,5 @@
 function prepareGifts(gifts) {
-  return Array.from(new Set(gifts)).sort((a, b) => a - b);
+  return [...new Set(gifts)].sort((a, b) => a - b);
 }
 
 const gifts1 = [3, 1, 2, 3, 4, 2, 5];
@@ -16,5 +16,5 @@ console.log(preparedGifts3); // []
 // There are no gifts, the list remains empty
 
 /* 
-    Score: ★★★★☆
+    Score: ★★★★★
 */
